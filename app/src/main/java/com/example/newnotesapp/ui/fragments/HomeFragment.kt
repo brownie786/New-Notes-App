6d2c0efe1777bbb379394a52ce5760c4ea885852
Aditya Tripathi <adityatripathik@gmail.com>
2025-01-19 17:04:37 +0530

    fourth commit

--- app/src/main/java/com/example/newnotesapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/newnotesapp/ui/fragments/HomeFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.newnotesapp.R
+import com.example.newnotesapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+
+        binding.btnAddNotes.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNotesFragment)
+        }
     }
 }
