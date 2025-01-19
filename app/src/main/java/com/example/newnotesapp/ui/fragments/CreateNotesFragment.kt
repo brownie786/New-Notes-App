8cd96d1cd73dd48353746f73a89d3e0bb07f5b35
Aditya Tripathi <adityatripathik@gmail.com>
2025-01-20 00:00:57 +0530

    fifth commit

--- app/src/main/java/com/example/newnotesapp/ui/fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/example/newnotesapp/ui/fragments/CreateNotesFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.newnotesapp.R
+import com.example.newnotesapp.databinding.FragmentCreateNotesBinding
 
 class CreateNotesFragment : Fragment() {
+
+    private lateinit var binding: FragmentCreateNotesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+        binding = FragmentCreateNotesBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
